0065ea73dceb5cb676b46af7be71c8311f13dda2
Martin UA <martinurieladler1@gmail.com>
2024-05-27 15:48:09 -0300

    LoginManager Script created and Btn_Aceptar connected

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    public InputField InputTexto;
+    public string contraseña;
+    //CUANDO EL USUARIO APRIETE EL BOTON BTN_ACEPTAR; DEBE DESCRIBIRSE UN MENSAJE EN LA CONSOLA
+    //SI EL TEXTO DEL INPUT PASSWORD COINCIDE CON UNA CONTRASEÑA PREDEFIDINA, POR NOSTROS
+    //ESCRIBIR "Access Granted", sino ESCRIBIR "Access denied"
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Btn_Aceptar()
+    {
+        if (InputTexto.text == contraseña)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
     }
 }
